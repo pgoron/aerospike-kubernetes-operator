c82c38fd57aa954db95ecbb3cbfb3dc77251f9d8
sud82 <sudhanshu.ranjan.82@gmail.com>
2021-02-03 01:42:13 -0800

    Upgraded operator-sdk to 0.18

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -26,10 +27,8 @@ import (
 	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	kubemetrics "github.com/operator-framework/operator-sdk/pkg/kube-metrics"
 	"github.com/operator-framework/operator-sdk/pkg/leader"
-	"github.com/operator-framework/operator-sdk/pkg/metrics"
 
-	//restmapper "sigs.k8s.io/controller-runtime/pkg/client/apiutil"
-	"github.com/operator-framework/operator-sdk/pkg/restmapper"
+	//"github.com/operator-framework/operator-sdk/pkg/restmapper"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
 	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
 	appsv1 "k8s.io/api/apps/v1"
@@ -38,7 +37,6 @@ import (
 	k8v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8Runtime "k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	crclient "sigs.k8s.io/controller-runtime/pkg/client"
@@ -188,8 +186,8 @@ func main() {
 
 	// Create a new Cmd to provide shared dependencies and start components
 	options := manager.Options{
-		Scheme:             scheme,
-		MapperProvider:     restmapper.NewDynamicRESTMapper,
+		Scheme: scheme,
+		// MapperProvider:     restmapper.NewDynamicRESTMapper,
 		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
 		NewClient:          newClient,
 		SyncPeriod:         d,
@@ -223,33 +221,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = serveCRMetrics(cfg); err != nil {
-		logger.Info("Could not generate and serve custom resource metrics", log.Ctx{"err": err})
-	}
-
-	// Add to the below struct any other metrics ports you want to expose.
-	servicePorts := []corev1.ServicePort{
-		{Port: metricsPort, Name: metrics.OperatorPortName, Protocol: corev1.ProtocolTCP, TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: metricsPort}},
-		{Port: operatorMetricsPort, Name: metrics.CRPortName, Protocol: corev1.ProtocolTCP, TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: operatorMetricsPort}},
-	}
-	// Create Service object to expose the metrics port(s).
-	_, err = metrics.CreateMetricsService(ctx, cfg, servicePorts)
-	if err != nil {
-		logger.Info("Could not create metrics Service", log.Ctx{"err": err})
-	}
-
-	// // CreateServiceMonitors will automatically create the prometheus-operator ServiceMonitor resources
-	// // necessary to configure Prometheus to scrape metrics from this operator.
-	// services := []*corev1.Service{service}
-	// _, err = metrics.CreateServiceMonitors(cfg, namespace, services)
-	// if err != nil {
-	// 	logger.Info("Could not create ServiceMonitor object", log.Ctx{"err": err})
-	// 	// If this operator is deployed to a cluster without the prometheus-operator running, it will return
-	// 	// ErrServiceMonitorNotPresent, which can be used to safely skip ServiceMonitor creation.
-	// 	if err == metrics.ErrServiceMonitorNotPresent {
-	// 		logger.Info("Install prometheus-operator in your cluster to create ServiceMonitor objects", log.Ctx{"err": err})
-	// 	}
-	// }
+	// Add the Metrics Service
+	addMetrics(ctx, cfg)
 
 	operatorNs, err := k8sutil.GetOperatorNamespace()
 	if err != nil {
@@ -287,22 +260,68 @@ func newClient(cache cache.Cache, config *rest.Config, options crclient.Options)
 	return crclient.New(config, options)
 }
 
+// addMetrics will create the Services and Service Monitors to allow the operator export the metrics by using
+// the Prometheus operator
+func addMetrics(ctx context.Context, cfg *rest.Config) {
+	// Get the namespace the operator is currently deployed in.
+	operatorNs, err := k8sutil.GetOperatorNamespace()
+	if err != nil {
+		if errors.Is(err, k8sutil.ErrRunLocal) {
+			log.Info("Skipping CR metrics server creation; not running in a cluster.")
+			return
+		}
+	}
+
+	if err := serveCRMetrics(cfg, operatorNs); err != nil {
+		log.Info("Could not generate and serve custom resource metrics", "error", err.Error())
+	}
+
+	// // Add to the below struct any other metrics ports you want to expose.
+	// servicePorts := []v1.ServicePort{
+	// 	{Port: metricsPort, Name: metrics.OperatorPortName, Protocol: v1.ProtocolTCP, TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: metricsPort}},
+	// 	{Port: operatorMetricsPort, Name: metrics.CRPortName, Protocol: v1.ProtocolTCP, TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: operatorMetricsPort}},
+	// }
+
+	// // Create Service object to expose the metrics port(s).
+	// service, err := metrics.CreateMetricsService(ctx, cfg, servicePorts)
+	// if err != nil {
+	// 	log.Info("Could not create metrics Service", "error", err.Error())
+	// }
+
+	// // CreateServiceMonitors will automatically create the prometheus-operator ServiceMonitor resources
+	// // necessary to configure Prometheus to scrape metrics from this operator.
+	// services := []*v1.Service{service}
+
+	// // The ServiceMonitor is created in the same namespace where the operator is deployed
+	// _, err = metrics.CreateServiceMonitors(cfg, operatorNs, services)
+	// if err != nil {
+	// 	log.Info("Could not create ServiceMonitor object", "error", err.Error())
+	// 	// If this operator is deployed to a cluster without the prometheus-operator running, it will return
+	// 	// ErrServiceMonitorNotPresent, which can be used to safely skip ServiceMonitor creation.
+	// 	if err == metrics.ErrServiceMonitorNotPresent {
+	// 		log.Info("Install prometheus-operator in your cluster to create ServiceMonitor objects", "error", err.Error())
+	// 	}
+	// }
+}
+
 // serveCRMetrics gets the Operator/CustomResource GVKs and generates metrics based on those types.
 // It serves those metrics on "http://metricsHost:operatorMetricsPort".
-func serveCRMetrics(cfg *rest.Config) error {
-	// Below function returns filtered operator/CustomResource specific GVKs.
-	// For more control override the below GVK list with your own custom logic.
+func serveCRMetrics(cfg *rest.Config, operatorNs string) error {
+	// The function below returns a list of filtered operator/CR specific GVKs. For more control, override the GVK list below
+	// with your own custom logic. Note that if you are adding third party API schemas, probably you will need to
+	// customize this implementation to avoid permissions issues.
 	filteredGVK, err := k8sutil.GetGVKsFromAddToScheme(apis.AddToScheme)
 	if err != nil {
 		return err
 	}
-	// Get the namespace the operator is currently deployed in.
-	operatorNs, err := k8sutil.GetOperatorNamespace()
+
+	// The metrics will be generated from the namespaces which are returned here.
+	// NOTE that passing nil or an empty list of namespaces in GenerateAndServeCRMetrics will result in an error.
+	ns, err := kubemetrics.GetNamespacesForMetrics(operatorNs)
 	if err != nil {
 		return err
 	}
-	// To generate metrics in other namespaces, add the values below.
-	ns := []string{operatorNs}
+
 	// Generate and serve custom resource specific metrics.
 	err = kubemetrics.GenerateAndServeCRMetrics(cfg, ns, filteredGVK, metricsHost, operatorMetricsPort)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -5,77 +5,58 @@ go 1.13
 require (
 	github.com/aerospike/aerospike-management-lib v0.0.0-20201112142459-9dbbb66be120
 	github.com/ashishshinde/aerospike-client-go v3.0.4-0.20200924015406-d85b25081637+incompatible
+	github.com/docker/spdystream v0.0.0-20181023171402-6480d4af844c // indirect
+	github.com/emicklei/go-restful v2.11.1+incompatible // indirect
 	github.com/evanphx/json-patch v4.5.0+incompatible
+	github.com/ghodss/yaml v1.0.1-0.20190212211648-25d852aebe32 // indirect
 	github.com/go-openapi/spec v0.19.4
 	github.com/inconshreveable/log15 v0.0.0-20201112154412-8562bdadbbac
 	github.com/jinzhu/copier v0.2.3 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
-	github.com/operator-framework/operator-sdk v0.15.0
-	github.com/stretchr/testify v1.4.0
+	github.com/operator-framework/operator-sdk v0.18.0
+	github.com/stretchr/testify v1.5.1
 	github.com/tmc/scp v0.0.0-20170824174625-f7b48647feef // indirect
-	golang.org/x/crypto v0.0.0-20200220183623-bac4c82f6975
-	k8s.io/api v0.17.4
-	k8s.io/apimachinery v0.17.4
-	k8s.io/client-go v12.0.0+incompatible
-	k8s.io/kube-openapi v0.0.0-20191107075043-30be4d16710a
-	sigs.k8s.io/controller-runtime v0.4.0
-)
-
-// Pinned to kubernetes-1.16.2
-replace (
-	k8s.io/api => k8s.io/api v0.0.0-20191016110408-35e52d86657a
-	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.0.0-20191016113550-5357c4baaf65
-	k8s.io/apimachinery => k8s.io/apimachinery v0.0.0-20191004115801-a2eda9f80ab8
-	k8s.io/apiserver => k8s.io/apiserver v0.0.0-20191016112112-5190913f932d
-	k8s.io/cli-runtime => k8s.io/cli-runtime v0.0.0-20191016114015-74ad18325ed5
-	k8s.io/client-go => k8s.io/client-go v0.0.0-20191016111102-bec269661e48
-	k8s.io/cloud-provider => k8s.io/cloud-provider v0.0.0-20191016115326-20453efc2458
-	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.0.0-20191016115129-c07a134afb42
-	k8s.io/code-generator => k8s.io/code-generator v0.0.0-20191004115455-8e001e5d1894
-	k8s.io/component-base => k8s.io/component-base v0.0.0-20191016111319-039242c015a9
-	k8s.io/cri-api => k8s.io/cri-api v0.0.0-20190828162817-608eb1dad4ac
-	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.0.0-20191016115521-756ffa5af0bd
-	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.0.0-20191016112429-9587704a8ad4
-	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.0.0-20191016114939-2b2b218dc1df
-	k8s.io/kube-proxy => k8s.io/kube-proxy v0.0.0-20191016114407-2e83b6f20229
-	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.0.0-20191016114748-65049c67a58b
-	k8s.io/kubectl => k8s.io/kubectl v0.0.0-20191016120415-2ed914427d51
-	k8s.io/kubelet => k8s.io/kubelet v0.0.0-20191016114556-7841ed97f1b2
-	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.0.0-20191016115753-cf0698c3a16b
-	k8s.io/metrics => k8s.io/metrics v0.0.0-20191016113814-3b1a734dba6e
-	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.0.0-20191016112829-06bb3c9d77c9
-)
-
-require (
-	github.com/Azure/go-autorest v12.2.0+incompatible // indirect
-	github.com/DATA-DOG/go-sqlmock v1.4.1 // indirect
-	github.com/Jeffail/gabs v1.4.0 // indirect
-	github.com/coreos/prometheus-operator v0.38.0 // indirect
-	github.com/helm/helm-2to3 v0.5.1 // indirect
-	github.com/magefile/mage v1.9.0 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
-	github.com/onsi/ginkgo v1.11.0 // indirect
-	github.com/onsi/gomega v1.8.1 // indirect
-	github.com/operator-framework/operator-lifecycle-manager v0.0.0-20200321030439-57b580e57e88 // indirect
-	github.com/operator-framework/operator-registry v1.6.2-0.20200330184612-11867930adb5 // indirect
-	github.com/pavel-v-chernykh/keystore-go v2.1.0+incompatible // indirect
-	github.com/prometheus/client_golang v1.5.1 // indirect
-	github.com/sirupsen/logrus v1.5.0 // indirect
 	github.com/yuin/gopher-lua v0.0.0-20200816102855-ee81675732da // indirect
-	go.uber.org/zap v1.14.1 // indirect
-	golang.org/x/tools v0.0.0-20200327195553-82bb89366a1e // indirect
-	gomodules.xyz/jsonpatch/v3 v3.0.1 // indirect
-	k8s.io/apiextensions-apiserver v0.17.4 // indirect
-	k8s.io/cli-runtime v0.17.4 // indirect
-	k8s.io/code-generator v0.17.4 // indirect
-	k8s.io/kubectl v0.17.4 // indirect
-	k8s.io/kubernetes v1.17.4 // indirect
-	sigs.k8s.io/controller-tools v0.2.8 // indirect
+	golang.org/x/crypto v0.0.0-20200414173820-0848c9571904
+	k8s.io/api v0.18.2
+	k8s.io/apimachinery v0.18.2
+	k8s.io/client-go v12.0.0+incompatible
+	k8s.io/kube-openapi v0.0.0-20200121204235-bf4fb3bd569c
+	sigs.k8s.io/controller-runtime v0.6.0
 )
+
+// // Pinned to kubernetes-1.16.2
+// replace (
+// 	k8s.io/api => k8s.io/api v0.0.0-20191016110408-35e52d86657a
+// 	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.0.0-20191016113550-5357c4baaf65
+// 	k8s.io/apimachinery => k8s.io/apimachinery v0.0.0-20191004115801-a2eda9f80ab8
+// 	k8s.io/apiserver => k8s.io/apiserver v0.0.0-20191016112112-5190913f932d
+// 	k8s.io/cli-runtime => k8s.io/cli-runtime v0.0.0-20191016114015-74ad18325ed5
+// 	k8s.io/cloud-provider => k8s.io/cloud-provider v0.0.0-20191016115326-20453efc2458
+// 	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.0.0-20191016115129-c07a134afb42
+// 	k8s.io/code-generator => k8s.io/code-generator v0.0.0-20191004115455-8e001e5d1894
+// 	k8s.io/component-base => k8s.io/component-base v0.0.0-20191016111319-039242c015a9
+// 	k8s.io/cri-api => k8s.io/cri-api v0.0.0-20190828162817-608eb1dad4ac
+// 	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.0.0-20191016115521-756ffa5af0bd
+// 	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.0.0-20191016112429-9587704a8ad4
+// 	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.0.0-20191016114939-2b2b218dc1df
+// 	k8s.io/kube-proxy => k8s.io/kube-proxy v0.0.0-20191016114407-2e83b6f20229
+// 	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.0.0-20191016114748-65049c67a58b
+// 	k8s.io/kubectl => k8s.io/kubectl v0.0.0-20191016120415-2ed914427d51
+// 	k8s.io/kubelet => k8s.io/kubelet v0.0.0-20191016114556-7841ed97f1b2
+// 	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.0.0-20191016115753-cf0698c3a16b
+// 	k8s.io/metrics => k8s.io/metrics v0.0.0-20191016113814-3b1a734dba6e
+// 	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.0.0-20191016112829-06bb3c9d77c9
+// )
 
 replace github.com/docker/docker => github.com/moby/moby v0.7.3-0.20190826074503-38ab9da00309 // Required by Helm
 
 replace github.com/openshift/api => github.com/openshift/api v0.0.0-20190924102528-32369d4db2ad // Required until https://github.com/operator-framework/operator-lifecycle-manager/pull/1241 is resolved
+
+replace (
+	github.com/Azure/go-autorest => github.com/Azure/go-autorest v13.3.2+incompatible // Required by OLM
+	k8s.io/client-go => k8s.io/client-go v0.18.2 // Required by prometheus-operator
+)
 
 // require (
 // 	github.com/aerospike/aerospike-management-lib v0.0.0-20201112142459-9dbbb66be120
