437e23a47de8321350af3cc1979619e80eed07fd
n.fraison <n.fraison@criteo.com>
2022-01-11 11:26:19 +0100

    Add aerospike init image configuration in aerospike cluster crd

--- api/v1beta1/aerospikecluster_types.go
+++ api/v1beta1/aerospikecluster_types.go
@@ -38,6 +38,8 @@ type AerospikeClusterSpec struct {
 	Size int32 `json:"size"`
 	// Aerospike server image
 	Image string `json:"image"`
+	// Aerospike init image
+	InitImage string `json:"initImage,omitempty"`
 	// Storage specify persistent storage to use for the Aerospike pods.
 	Storage AerospikeStorageSpec `json:"storage,omitempty"`
 	// AerospikeAccessControl has the Aerospike roles and users definitions. Required if aerospike cluster security is enabled.
